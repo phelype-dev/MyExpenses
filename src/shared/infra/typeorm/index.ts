ab4fdd9570f93e7b36143b599898ae7883a332c0
phelype <phelype.dev@outlook.com>
2022-11-19 15:23:40 -0300

    Adicionado DB no elephantsql

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -8,7 +8,7 @@ dotenv.config();
 
 export const dataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
+  host: process.env.DB_HOST,
   port: 5432,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
