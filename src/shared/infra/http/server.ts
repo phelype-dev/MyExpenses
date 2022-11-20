b93f634ad0ed972e3375d1ade5a57b804c45ec28
phelype <phelype.dev@outlook.com>
2022-11-20 14:51:32 -0300

    Config heroku

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,10 +2,11 @@ import { AppError } from '@shared/errors/AppError';
 import { dataSource } from '../typeorm';
 import { app } from './app';
 
+const port = process.env.PORT || 3000;
 dataSource.initialize().then(async () => {
   try {
     const server = app.listen(3000, () => {
-      return console.log('Server started on port 3000! 🏆');
+      return console.info(`Server started on port ${port}! 🏆`);
     });
   } catch (e) {
     throw new AppError('Erro encontrado: ', e);
