eade1d89fc6bf765c9086ee7cd46489004f9fdc0
phelype <phelype.dev@outlook.com>
2022-11-22 13:08:44 -0300

    package

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,12 +2,12 @@ import { AppError } from '@shared/errors/AppError';
 import { dataSource } from '../typeorm';
 import { app } from './app';
 
-const port = process.env.PORT || '3000';
-console.log(port);
 dataSource.initialize().then(async () => {
   try {
-    const server = app.listen(() => {
-      return console.info(`Server started on port ${port}! 🏆`);
+    const server = app.listen(process.env.PORT || 8080, () => {
+      return console.info(
+        `Server started on port ${process.env.PORT || 8080}! 🏆`,
+      );
     });
   } catch (e) {
     throw new AppError('Erro encontrado: ', e);
