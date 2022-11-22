8208c8064a82759a7069c410449c767cf861804a
phelype <phelype.dev@outlook.com>
2022-11-22 12:56:21 -0300

    package

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,9 +3,10 @@ import { dataSource } from '../typeorm';
 import { app } from './app';
 
 const port = process.env.PORT || '3000';
+console.log(port);
 dataSource.initialize().then(async () => {
   try {
-    const server = app.listen(3000, () => {
+    const server = app.listen(() => {
       return console.info(`Server started on port ${port}! 🏆`);
     });
   } catch (e) {
